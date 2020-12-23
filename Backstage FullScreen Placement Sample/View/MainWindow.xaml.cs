833834e85aad6d339a8bc2e5cb0204fae75427e3
JagadeesanPichaimuthu <44426969+JagadeesanPichaimuthu@users.noreply.github.com>
2020-12-23 12:03:22 +0530

    Add files via upload

---
+++ Backstage FullScreen Placement Sample/View/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Syncfusion.Windows.Tools.Controls;
+using System.Windows;
+
+namespace BackStage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
